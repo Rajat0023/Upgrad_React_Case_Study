3485a3a984d0b1af2d1a1977fe1dce01b560bf79
Rajat0023 <rajatsingh2237@gmail.com>
2019-08-29 18:22:29 +0530

    Added Header for Login Page & Styling done

---
+++ src/screens/login/Login.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import './Login.css';
+
+class Login extends Component{
+    render(){
+        return (
+            <header className="app-header">
+
+            </header>
+        )
+    }
+}
+
+export default Login;
